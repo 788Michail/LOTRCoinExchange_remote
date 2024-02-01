127b44d17d3b62dc604d70b0604a53aa8448b58f
Michail Papasarris <788.michail@gmail.com>
2024-02-01 09:32:00 +0200

    Initial Commit

---
+++ LOTRCoinExchange/LOTRCoinExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRCoinExchangeApp.swift
+//  LOTRCoinExchange
+//
+//  Created by Michail on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRCoinExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
