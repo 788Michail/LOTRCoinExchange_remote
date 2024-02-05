c2b0d68963907be0cdb005dd675e5146ead7cf30
Michail Papasarris <788.michail@gmail.com>
2024-02-05 13:41:03 +0200

    Binding

---
+++ LOTRCoinExchange/IconGrid.swift
@@ -0,0 +1,18 @@
+//
+//  IconGrid.swift
+//  LOTRCoinExchange
+//
+//  Created by Michail on 5/2/24.
+//
+
+import SwiftUI
+
+struct IconGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconGrid()
+}
